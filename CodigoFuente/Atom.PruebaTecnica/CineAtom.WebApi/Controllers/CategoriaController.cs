80ff1465c2ced24c2b1384a2f2cb8ab9d5ce952e
EmmanuelBarboza <barbozaprogra18@gmail.com>
2025-06-24 13:49:30 -0600

    feat: Created CategoriaController

---
+++ CodigoFuente/Atom.PruebaTecnica/CineAtom.WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,34 @@
+﻿using CineAtom.WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineAtom.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : Controller
+    {
+        private readonly CineAtomDbContext _context;
+
+        public CategoriaController(CineAtomDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategorias()
+        {
+            try
+            {
+                var categorias = await _context.Categorias.ToListAsync();
+                return Ok(categorias);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al obtener las categorias: {ex.Message}");
+            }
+
+        }
+
+    }
+}
